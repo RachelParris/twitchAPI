20b1c7189b1909a8a4a55ca2353a9792dc8ad3e0
RachelParris <sonnet22@gmail.com>
2018-02-11 17:22:13 -0500

    Able to get JSON from API call.

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,25 @@
-$(document).ready(function() {});
+$(document).ready(() => {
+
+    // Execute API call
+	const liveStreams = ["freecodecamp", "legendoftotalwar"]
+
+	//Setup API call
+	liveStreams.forEach(liveStream => {
+		const queryURL = "https://wind-bow.glitch.me/twitch-api/streams/" + liveStream;
+		$.ajax({
+            url: queryURL,
+			method: "GET",
+			dataType: "json"
+        })
+            .done((res) => {
+				if (res.stream === null) {
+					console.log("User is offline.")
+				} else {
+					console.log("User " + res.stream.channel.display_name + " is online.")
+				}
+			})
+            .fail((err) => {
+				console.error("API call error: ", err)
+			})
+	});
+});
