eb0f60dee01f4f840723c041aa17d7986ee9f179
RachelParris <sonnet22@gmail.com>
2018-02-11 19:14:38 -0500

    App is functional - WIP

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Coming Soon.
 *CSS3
 *JavaScript
 *jQuery
+* AJAX
 
 ### Contributers
 
--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,10 @@
 $(document).ready(() => {
+	//TODO Need to add search functionality
 
-    // Execute API call
-	const liveStreams = ["freecodecamp", "legendoftotalwar"]
+    // List of live streams
+	const liveStreams = ["freecodecamp", "legendoftotalwar", "summit1g"]
 
-	//Setup API call
+	//Setup & Execute API Call
 	liveStreams.forEach(liveStream => {
 		const queryURL = "https://wind-bow.glitch.me/twitch-api/streams/" + liveStream;
 		$.ajax({
@@ -12,14 +13,55 @@ $(document).ready(() => {
 			dataType: "json"
         })
             .done((res) => {
-				if (res.stream === null) {
-					console.log("User is offline.")
-				} else {
-					console.log("User " + res.stream.channel.display_name + " is online.")
-				}
+				postResults(res);
 			})
             .fail((err) => {
 				console.error("API call error: ", err)
 			})
 	});
+
+	const postResults = (res) => {
+
+		if (res.stream === null) {
+
+			// TODO Use placeholder stuff for offline users
+			let newBox = $('<div class="item">');
+			let user = "User is offline.";
+			newBox.append(user);
+			$("#channels").prepend(newBox);
+
+		} else {
+
+			let channelName = res.stream.channel.name;
+			let logo = res.stream.channel.logo;
+			let game = res.stream.channel.game;
+			let gameStart = res.stream.created_at;
+			let currentViews = res.stream.viewers;
+			let rating = res.stream.channel.mature ? "Rated M" : ""; //boolean
+			let followers = res.stream.channel.followers;
+			let channelURL = res.stream.channel.url;
+
+			let newBox = $(
+				`<div class="item">
+					<div class="image">
+						<a href="${res.stream.channel.url}" target="_blank">
+							<img src="${logo}" alt="${channelName} logo">
+						</a>
+					</div>
+					<div class="info">
+						<h2>${channelName}</h2>
+						<h3>Live Steam started at ${gameStart}</h3>
+						<p>Currently playing ${game}<br>
+						${rating}<br>
+						Current views: ${currentViews} / Followers: ${followers}
+						</p>
+					</div>
+				</div>`
+			);
+
+			// newBox.append(user);
+			$("#channels").prepend(newBox);
+		}
+	}
 });
+
